e9a7ce31c6b05f58a8172a61ba76c552ea539e8b
DamonSFu <475774287@qq.com>
2021-07-21 07:48:11 +0800

    spring study  -- 静态代理（staticproxy）

--- src/main/java/staticproxy/UserServiceImpl.java
+++ src/main/java/staticproxy/UserServiceImpl.java
@@ -16,14 +16,14 @@ public class UserServiceImpl implements UserService {
 //            e.printStackTrace();
 //        }
         // 当有代理后，重复冗余的事务性代码可以交给代理去完成，只用留下处理业务逻辑代码，以下同理
-        System.out.println("处理业务逻辑,调用DAO~~~");
+        System.out.println("处理业务逻辑,调用 save DAO~~~");
     }
 
     @Override
     public void delete(String id) {
 //        try {
 //            System.out.println("开启事务");
-            System.out.println("处理业务逻辑,调用DAO~~~");
+            System.out.println("处理业务逻辑,调用 delete DAO~~~");
 //            System.out.println("提交事务");
 //        }catch (Exception e){
 //            System.out.println("回滚事务");
@@ -35,7 +35,7 @@ public class UserServiceImpl implements UserService {
     public void update() {
 //        try {
 //            System.out.println("开启事务");
-            System.out.println("处理业务逻辑,调用DAO~~~");
+            System.out.println("处理业务逻辑,调用update DAO~~~");
 //            System.out.println("提交事务");
 //        }catch (Exception e){
 //            System.out.println("回滚事务");
@@ -47,7 +47,7 @@ public class UserServiceImpl implements UserService {
     public String findAll(String name) {
 //        try {
 //            System.out.println("开启事务");
-            System.out.println("处理业务逻辑,调用DAO~~~");
+            System.out.println("处理业务逻辑,调用findAll DAO~~~");
 //            System.out.println("提交事务");
 //        }catch (Exception e){
 //            System.out.println("回滚事务");
@@ -60,7 +60,7 @@ public class UserServiceImpl implements UserService {
     public String findOne(String id) {
 //        try {
 //            System.out.println("开启事务");
-            System.out.println("处理业务逻辑,调用DAO~~~");
+            System.out.println("处理业务逻辑,调用findOne DAO~~~");
 //            System.out.println("提交事务");
 //        }catch (Exception e){
 //            System.out.println("回滚事务");
